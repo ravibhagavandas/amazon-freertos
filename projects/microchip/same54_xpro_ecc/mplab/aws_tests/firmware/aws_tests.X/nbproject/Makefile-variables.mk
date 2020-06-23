af020c6b2aabc4656483af1f3c8a20ab8c021c94
Shanmugasundaram Viswanathan <shanmugasundaram.viswanathan@microchip.com>
2020-06-23 12:28:37 +0530

    Updated SAME54 projects to use latest CryptoAuthLib

---
+++ projects/microchip/same54_xpro_ecc/mplab/aws_tests/firmware/aws_tests.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# aws_tests configuration
+CND_ARTIFACT_DIR_aws_tests=dist/aws_tests/production
+CND_ARTIFACT_NAME_aws_tests=aws_tests.X.production.hex
+CND_ARTIFACT_PATH_aws_tests=dist/aws_tests/production/aws_tests.X.production.hex
+CND_PACKAGE_DIR_aws_tests=${CND_DISTDIR}/aws_tests/package
+CND_PACKAGE_NAME_aws_tests=awstests.x.tar
+CND_PACKAGE_PATH_aws_tests=${CND_DISTDIR}/aws_tests/package/awstests.x.tar
